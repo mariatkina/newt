7fca43cc003abc4043b93dc81d61ec7cdd50bc99
Evg Mariatkina <e.mariatkina@gmail.com>
2023-08-16 09:08:23 +0300

    16-08

---
+++ src/GetOneRequest/BookingClass.java
@@ -0,0 +1,106 @@
+package GetOneRequest;
+
+public class BookingClass {
+    private String firstname;
+    private String lastname;
+    private int totalprice;
+    private boolean depositpaid;
+    private bookingdates bookingdates;
+    private String additionalneeds;
+
+    public BookingClass() {
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getTotalprice() {
+        return totalprice;
+    }
+
+    public void setTotalprice(int totalprice) {
+        this.totalprice = totalprice;
+    }
+
+    public boolean isDepositpaid() {
+        return depositpaid;
+    }
+
+    public void setDepositpaid(boolean depositpaid) {
+        this.depositpaid = depositpaid;
+    }
+
+    public bookingdates getBookingdates() {
+        return bookingdates;
+    }
+
+    public void setBookingdates(bookingdates bookingdates) {
+        this.bookingdates = bookingdates;
+    }
+
+    public String getAdditionalneeds() {
+        return additionalneeds;
+    }
+
+    public void setAdditionalneeds(String additionalneeds) {
+        this.additionalneeds = additionalneeds;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", totalprice=" + totalprice +
+                ", depositpaid=" + depositpaid +
+                ", bookingdates=" + bookingdates +
+                ", additionalneeds='" + additionalneeds + '\'' +
+                '}';
+    }
+}
+class bookingdates{
+    private String checkin;
+    private String checkout;
+
+    public bookingdates() {
+    }
+
+    public bookingdates(String checkin, String checkout) {
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+
+    public String getCheckin() {
+        return checkin;
+    }
+
+    public void setCheckin(String checkin) {
+        this.checkin = checkin;
+    }
+
+    public String getCheckout() {
+        return checkout;
+    }
+
+    public void setCheckout(String checkout) {
+        this.checkout = checkout;
+    }
+
+    @Override
+    public String toString() {
+        return "checkout: '" + checkout + '\'';
+    }
+}
